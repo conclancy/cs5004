a923d35eaf5d7dc85df30701f48ce6b5332021d8
Connor Clancy <cclancy@wayfair.com>
2022-10-04 09:48:42 +0100

    Add overlap method and initial tests.

--- homework/hw02Rectangles/src/Rectangle.java
+++ homework/hw02Rectangles/src/Rectangle.java
@@ -1,3 +1,5 @@
+import java.util.NoSuchElementException;
+
 /**
  * This class implments a rectangle with x, y coordinates and a width and height.
  */
@@ -15,7 +17,7 @@ public class Rectangle {
    * @param w width of the rectangle
    * @param h height of the rectangle
    */
-  public Rectangle(double x, double y, double w, double h) {
+  public Rectangle(double x, double y, double w, double h) throws IllegalArgumentException {
     if (w<0 || h<0) {
       throw new IllegalArgumentException("Rectangles must have positive width and height.");
     }
@@ -24,4 +26,37 @@ public class Rectangle {
     this.w = w;
     this.h = h;
   }
+
+  /**
+   * A method to determine overlap with another Rectangle object.
+   *
+   * @param other another rectangle
+   * @return boolean
+   */
+  public boolean overlap(Rectangle other) {
+    return (x < other.x + other.w && x + w > other.x && y < other.y + other.h && y + h > other.y);
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
--- homework/hw02Rectangles/test/RectangleTest.java
+++ homework/hw02Rectangles/test/RectangleTest.java
@@ -1,3 +1,47 @@
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.w3c.dom.css.Rect;
+
+/**
+ * A JUnit test for the Rectangle class.
+ */
 public class RectangleTest {
+
+  private Rectangle rectangleIllegal;
+  private Rectangle rectangleSmall;
+  private Rectangle rectangleLarge;
+  private Rectangle rectangleSmallFalse;
+
+  /**
+   * Set up testing objects.
+   */
+  @Before
+  public void setUp() {
+    rectangleSmall = new Rectangle(0,0,1,2);
+    rectangleLarge = new Rectangle(0,0,100,100);
+    rectangleSmallFalse = new Rectangle(2,2,1,1);
+  }
+
+  /**
+   * Test illegal setup error.
+   */
+  @Test(expected = IllegalArgumentException.class)
+  public void testIllegalDimensions() {
+    rectangleIllegal = new Rectangle(0,0,-1,-1);
+  }
+
+  /**
+   * Test overlap method.
+   */
+  @Test
+  public void testOverlap() {
+    assertEquals(true, rectangleSmall.overlap(rectangleLarge));
+    assertEquals(true, rectangleLarge.overlap(rectangleSmall));
+    assertEquals(false, rectangleSmall.overlap(rectangleSmallFalse));
+    assertEquals(true, rectangleSmallFalse.overlap(rectangleLarge));
+  }
 
 }
