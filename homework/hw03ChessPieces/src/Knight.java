734a5c0ae2b4b0c7a76e4ab4e536e495f12b4e8f
Connor Clancy <clancy.mu@gmail.com>
2022-10-12 18:18:50 -0400

    Add Knight class

---
+++ homework/hw03ChessPieces/src/Knight.java
@@ -0,0 +1,26 @@
+public class Knight extends AbstractChessPiece {
+
+  /**
+   * Constructor for a knight piece.
+   *
+   * @param row    row of the piece
+   * @param column column of the piece
+   * @param color  of the chess piece
+   */
+  public Knight(int row, int column, Color color) {
+    super(row, column, color);
+  }
+
+  /**
+   * Determines if the chess piece can move to a specified cell.
+   *
+   * @param cell the cell for which to test a move
+   * @return true if the move is valid
+   */
+  @Override
+  public boolean canMove(Cell cell) {
+    return
+        (Math.abs(super.getRow() - cell.getRow()) * Math.abs(super.getColumn() - cell.getColumn()))
+            == 2;
+  }
+}
