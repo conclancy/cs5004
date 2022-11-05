1e16b11029e207b8ce915af9dcf3c8112638199a
Connor Clancy <clancy.mu@gmail.com>
2022-11-05 15:28:39 -0400

    Initial commit.
    Added all classes and began writing tests.

---
+++ labs/lab5/test/SentenceListTest.java
@@ -0,0 +1,11 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Unit tests for the Sentence class.
+ */
+public class SentenceListTest {
+
+}
