e5e28dd8937d07afb482ae856943e07bb5514a18
Connor Clancy <cclancy@wayfair.com>
2022-11-26 11:32:20 -0800

    Add `equals` and `hashCode` methods.
    Override default `equals` and `hashCode` methods to ensure we can properly identify the equality of two Point2D objects.

--- final/src/model/Point2D.java
+++ final/src/model/Point2D.java
@@ -1,5 +1,7 @@
 package model;
 
+import java.util.Objects;
+
 /**
  * This class represents a 2D point. This point is denoted in Cartesian coordinates as (x,y).
  */
@@ -44,5 +46,34 @@ public class Point2D {
    */
   public double getY() {
     return y;
+  }
+
+  /**
+   * Determines if two Point2D objects are equal to one another.
+   *
+   * @param other the other object to be compared.
+   * @return true if the objects are the same
+   */
+  @Override
+  public boolean equals(Object other) {
+    if (this==other) {
+      return true;
+    }
+    if (!(other instanceof Point2D other2D)) {
+      return false; //other cannot be equal to this as it is not a Person object!
+    }
+
+    return this.getX() == other2D.getX()
+        && this.getY() == other2D.getY();
+  }
+
+  /**
+   * Create a hash table of a Point2D object.
+   *
+   * @return Point2D hash table.
+   */
+  @Override
+  public int hashCode() {
+    return Objects.hash(this.x, ",", this.y);
   }
 }
