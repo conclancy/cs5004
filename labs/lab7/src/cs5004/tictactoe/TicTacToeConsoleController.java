85542b4f193cb588aec4f7839d555b3ce79a2743
Connor Clancy <cclancy@wayfair.com>
2022-12-02 13:36:45 -0500

    Add the Console Controller class.

---
+++ labs/lab7/src/cs5004/tictactoe/TicTacToeConsoleController.java
@@ -0,0 +1,15 @@
+package cs5004.tictactoe;
+
+public class TicTacToeConsoleController implements TicTacToeController {
+
+  /**
+   * Execute a single game of tic tac toe given a tic tac toe Model. When the game is over, the
+   * playGame method ends.
+   *
+   * @param m a non-null tic tac toe Model
+   */
+  @Override
+  public void playGame(TicTacToe m) {
+
+  }
+}
