e4735b1d144d49f3b72203a1e35f2f76378dea15
Connor Clancy <cclancy@wayfair.com>
2022-12-02 14:49:15 -0500

    Add game play logic.

--- labs/lab7/src/cs5004/tictactoe/TicTacToeConsoleController.java
+++ labs/lab7/src/cs5004/tictactoe/TicTacToeConsoleController.java
@@ -6,78 +6,88 @@ import java.util.Scanner;
 
 public class TicTacToeConsoleController implements TicTacToeController {
 
-  private final Appendable out;
+  private final Appendable appendable;
   private final Scanner scan;
 
-  public TicTacToeConsoleController(Readable in, Appendable out) {
-    if (in == null || out == null) {
+  /**
+   * @param readable   the input values to run the game.
+   * @param appendable the output values for the game.
+   * @throws IllegalArgumentException if either of the params are null when passed.
+   * @throws IllegalStateException    if more than all the spaces on the board are filled, and we
+   *                                  try to add another move.
+   */
+  public TicTacToeConsoleController(Readable readable, Appendable appendable)
+      throws IllegalArgumentException, IllegalStateException {
+    if (readable == null || appendable == null) {
       throw new IllegalArgumentException("Values cannot be null!");
     }
-    this.out = out;
-    scan = new Scanner(in);
+    this.appendable = appendable;
+    scan = new Scanner(readable);
   }
 
   /**
-   * Execute a single game of tic tac toe given a tic tac toe Model. When the game is over, the
+   * Execute a single game of tic-tac-toe given a tic-tac-toe Model. When the game is over, the
    * playGame method ends.
    *
    * @param m a non-null tic tac toe Model
+   * @throws IllegalStateException if more than all the spaces on the board are filled, and we try
+   *                               to add another move.
    */
   @Override
-  public void playGame(TicTacToe m) {
+  public void playGame(TicTacToe m) throws IllegalStateException {
     try {
       playGameHelper(m);
       Integer row = null;
       Integer col = null;
-      String token = "";
-
+      String play = "";
 
       while (!m.isGameOver()) {
-        token = scan.next();
-        if (token.equalsIgnoreCase("q")) {
+        play = scan.next();
+        if (play.equalsIgnoreCase("q")) {
           break;
         }
 
         try {
-          int someInt = Integer.parseInt(token);
+          int i = Integer.parseInt(play);
           if (row == null) {
-            row = someInt;
+            row = i;
           } else {
-            col = someInt;
+            col = i;
             m.move(row - 1, col - 1);
             if (m.isGameOver()) {
-              out.append(m.toString()).append("\n");
-              out.append("Game is over! ");
+              appendable.append(m.toString()).append("\n");
+              appendable.append("Game is over! ");
               if (m.getWinner() != null) {
-                out.append(m.getWinner().toString() + " wins.\n");
+                appendable.append(m.getWinner().toString()).append(" wins!\n");
               } else {
-                out.append("Tie game.\n");
+                appendable.append("Tie game.\n");
               }
               break;
             }
             playGameHelper(m);
-            row = col = null;
+            row = null;
+            col = null;
           }
         } catch (NumberFormatException e) {
-          out.append("This is not a valid number: " + token).append("\n");
+          appendable.append("This is not a valid number: ").append(play).append("\n");
         }
       }
 
-      if (!m.isGameOver() && token.equalsIgnoreCase("q")) {
-        out.append("Game quit! Ending game state:\n" + m.toString() + "\n");
+      if (!m.isGameOver() && play.equalsIgnoreCase("q")) {
+        appendable.append("Game quit! Ending game state:\n").append(m.toString()).append("\n");
       } else if (!m.isGameOver()) {
         throw new IllegalStateException("Ran out of inputs");
       }
     } catch (IOException e) {
       scan.close();
-      throw new IllegalStateException("append failed", e);
+      throw new IllegalStateException("Append failed:", e);
     } catch (NoSuchElementException e) {
-      throw new IllegalStateException("failed to read from readable");
+      throw new IllegalStateException("Failed to read from readable");
     }
   }
 
-  private void playGameHelper(TicTacToe m) throws IOException {
-    out.append(m.toString()).append("\n");
-    out.append("Enter a move for " + m.getTurn().toString()).append(":\n");
+  private void playGameHelper(TicTacToe t) throws IOException {
+    appendable.append(t.toString()).append("\n");
+    appendable.append("Enter a move for ").append(t.getTurn().toString()).append(":\n");
   }
 }
