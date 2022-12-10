15a9bc0705253609136fe89e69cbd785086cca7f
Connor Clancy <clancy.mu@gmail.com>
2022-12-10 15:17:51 -0500

    Update JavaDocs

--- final/src/cs5004/animator/view/FrameChange.java
+++ final/src/cs5004/animator/view/FrameChange.java
@@ -1,7 +1,7 @@
 package cs5004.animator.view;
 
 /**
- * Represents the type of change being made. //TODO: Complete this for assignment part 3.
+ * Represents the type of change being made.
  */
 public enum FrameChange {
   EDIT, ADD, DELETE
